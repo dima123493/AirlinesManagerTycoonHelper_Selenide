657a7eb16bba018543589b28e88a92313579b20a
Dmytro Zubenko <fokist@outlook.com>
2023-04-09 17:10:36 +0300

    Audit condition checker and its implementer

--- src/main/java/pages/RouteDetailsPage.java
+++ src/main/java/pages/RouteDetailsPage.java
@@ -1,55 +1,61 @@
 package pages;
 
-import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
 
 import java.util.List;
 import java.util.Map;
 
-import static com.codeborne.selenide.Selenide.$x;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class RouteDetailsPage {
-    public final static String airportName = "//*[@id=\"box2\"]/li[4]/b";
+    public static final String airportName = "//*[@id=\"box2\"]/li[4]/b";
 
-    public final static String economyDemand = "//*[@id=\"marketing_linePricing\"]/div[3]/div[1]/div[3]/b";
-    public final static String businessDemand = "//*[@id=\"marketing_linePricing\"]/div[3]/div[2]/div[3]/b";
-    public final static String firstDemand = "//*[@id=\"marketing_linePricing\"]/div[3]/div[3]/div[3]/b";
-    public final static String cargoDemand = "//*[@id=\"marketing_linePricing\"]/div[3]/div[4]/div[3]/b";
+    public static final String economyDemand = "//*[@id=\"marketing_linePricing\"]/div[3]/div[1]/div[3]/b";
+    public static final String businessDemand = "//*[@id=\"marketing_linePricing\"]/div[3]/div[2]/div[3]/b";
+    public static final String firstDemand = "//*[@id=\"marketing_linePricing\"]/div[3]/div[3]/div[3]/b";
+    public static final String cargoDemand = "//*[@id=\"marketing_linePricing\"]/div[3]/div[4]/div[3]/b";
 
-    public final static String economyPrice = "//*[@id=\"marketing_linePricing\"]/div[3]/div[1]/div[2]/b";
-    public final static String businessPrice = "//*[@id=\"marketing_linePricing\"]/div[3]/div[2]/div[2]/b";
-    public final static String firstPrice = "//*[@id=\"marketing_linePricing\"]/div[3]/div[3]/div[2]/b";
-    public final static String cargoPrice = "//*[@id=\"marketing_linePricing\"]/div[3]/div[4]/div[2]/b";
-
-    public final static SelenideElement linkToPricesPage = $x("//*[@id=\"showLine\"]/div[5]/div/a");
-
-
+    public static final String economyPrice = "//*[@id=\"marketing_linePricing\"]/div[3]/div[1]/div[2]/b";
+    public static final String businessPrice = "//*[@id=\"marketing_linePricing\"]/div[3]/div[2]/div[2]/b";
+    public static final String firstPrice = "//*[@id=\"marketing_linePricing\"]/div[3]/div[3]/div[2]/b";
+    public static final String cargoPrice = "//*[@id=\"marketing_linePricing\"]/div[3]/div[4]/div[2]/b";
+    public static final String linkToPricesPage = "//*[@id=\"showLine\"]/div[5]/div/a";
     public void collectDemandValueAndPrices(Map<String, String> nameOfAirportAndLink, List<List<String>> rows) {
         for (Map.Entry<String, String> entry : nameOfAirportAndLink.entrySet()) {
             String name = entry.getKey();
             open(entry.getValue());
 
-            String ecoDemand = $x(economyDemand).getText().trim();
-            String businDemand = $x(businessDemand).getText().trim();
-            String frtDemand = $x(firstDemand).getText().trim();
-            String cargDemand = $x(cargoDemand).getText().trim();
+            //here page can be reloaded (should be checked)
+            RoutePricePage action = new RoutePricePage();
+            action.auditProcedure();
+            //here page can be reloaded (should be checked)
 
-            String ecoPrice = $x(economyPrice).getText().trim();
-            String businPrice = $x(businessPrice).getText().trim();
-            String frtPrice = $x(firstPrice).getText().trim();
-            String cargPrice = $x(cargoPrice).getText().trim();
+            String ecoDemand = $x(economyDemand).getText().trim().replaceAll("\\D+", "");
+            String businDemand = $x(businessDemand).getText().trim().replaceAll("\\D+", "");
+            String frtDemand = $x(firstDemand).getText().trim().replaceAll("\\D+", "");
+            String cargDemand = $x(cargoDemand).getText().trim().replaceAll("\\D+", "");
 
-            String newEcoDemand = ecoDemand.replaceAll("\\D+", "");
+            String ecoPrice = $x(economyPrice).getText().trim().replaceAll("\\D+", "");
+            String businPrice = $x(businessPrice).getText().trim().replaceAll("\\D+", "");
+            String frtPrice = $x(firstPrice).getText().trim().replaceAll("\\D+", "");
+            String cargPrice = $x(cargoPrice).getText().trim().replaceAll("\\D+", "");
+
+/*            String newEcoDemand = ecoDemand.replaceAll("\\D+", "");
             String newBusinDemand = businDemand.replaceAll("\\D+", "");
             String newFrtDemand = frtDemand.replaceAll("\\D+", "");
             String newCargDemand = cargDemand.replaceAll("\\D+", "");
             String newEcoPrice = ecoPrice.replaceAll("\\D+", "");
             String newbusinPrice = businPrice.replaceAll("\\D+", "");
             String newFrtPrice = frtPrice.replaceAll("\\D+", "");
-            String newCargPrice = cargPrice.replaceAll("\\D+", "");
+            String newCargPrice = cargPrice.replaceAll("\\D+", "");*/
 
-            rows.add(List.of(name, newEcoDemand, newBusinDemand, newFrtDemand, newCargDemand, newEcoPrice, newbusinPrice, newFrtPrice, newCargPrice));
-
+            rows.add(List.of(name, ecoDemand, businDemand, frtDemand, cargDemand, ecoPrice, businPrice, frtPrice, cargPrice));
         }
+    }
+
+    public void collectAirPotNamesAndLinks(Map<String, String> nameOfAirportAndLink) {
+        String portName = $x(airportName).getText().trim().toUpperCase().substring(0, 3);
+        String link = $x(linkToPricesPage).getAttribute("href");
+        nameOfAirportAndLink.put(portName, link);
     }
 }
--- src/main/java/pages/RoutePricePage.java
+++ src/main/java/pages/RoutePricePage.java
@@ -1,10 +1,20 @@
 package pages;
 
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
+import static java.lang.Long.parseLong;
 
 public class RoutePricePage {
+    String reliability = "//*[@id=\"marketing_linePricing\"]/div[5]/span[2]/p/a";
+    String reliabilityStateRU = "Очень ненадежный";
+    String reliabilityStateEN = "Very unreliable";
+    String amountOfMoneyInMyAccount = "ressource3";
     String performAudit = "//*[@id=\"marketing_linePricing\"]/div[1]/a/span";//hover,click
+    String auditPrice = "//*[@id=\"popupContainer\"]/div[2]/p/span";//hover,click
     String performAuditButton = "//div[@id='popupContainer']//span[@id='internalAuditButton']";
+    String declineAuditButton = "//*[@id=\"popupContainer\"]/div[3]/span";
 
     String economyClassPrice = "line_priceEco";
     String businessClassPrice = "line_priceBus";
@@ -12,16 +22,49 @@ public class RoutePricePage {
     String cargoPrice = "line_priceCargo";
     String submitPricesButton = "//*[@id=\"marketing_linePricing\"]/form/div/input";
 
-    public void performAudit(){
+    public void auditAnalysis() {
         $x(performAudit).hover().click();
+    }
+
+    public void performAudit() {
         $x(performAuditButton).hover().click();
     }
 
-    public void applyNewPrices(String economyPrice,String businessPrice,String firstPrice,String crgPrice ){
-        $x(economyClassPrice).setValue(economyPrice);
-        $x(businessClassPrice).setValue(businessPrice);
-        $x(firstClassPrice).setValue(firstPrice);
-        $x(cargoPrice).setValue(crgPrice);
+    public void applyNewPrices(String economyPrice, String businessPrice, String firstPrice, String crgPrice) {
+        $(By.id(economyClassPrice)).setValue(economyPrice);
+        $(By.id(businessClassPrice)).setValue(businessPrice);
+        $(By.id(firstClassPrice)).setValue(firstPrice);
+        $(By.id(cargoPrice)).setValue(crgPrice);
         $x(submitPricesButton).click();
     }
+
+    public void auditProcedure() {
+        long myAccountMoney;
+        long auditPriceValue;
+        if ($x(reliability).getText().equals(reliabilityStateRU) ||
+                $x(reliability).getText().equals(reliabilityStateEN)) {
+            myAccountMoney = parseLong(($(By.id(amountOfMoneyInMyAccount)).getText().replaceAll("(?<=.)\\D+","")));
+            auditAnalysis();
+            auditPriceValue = parseLong($x(auditPrice).getText().trim().replaceAll("\\D+", ""));
+            if (auditPriceValue < myAccountMoney) {
+                performAudit();
+            } else {
+                $x(declineAuditButton).hover().click();
+            }
+        }
+    }
+
+/*    public static void main(String[] args) {
+        long test1 = parseLong("-33,696,140,341".replaceAll("(?<=.)\\D+",""));
+        long test3 = parseLong("33,696,140,341".replaceAll("(?<=.)\\D+",""));
+        long test2 = parseLong("140,341".replaceAll("\\D+", ""));
+        System.out.println(test1);
+        System.out.println(test2);
+        if(test1>test2){
+            System.out.println("cool. audit can be performed");
+        }else{
+            System.out.println("not enough money");
+        }
+    }*/
+
 }
