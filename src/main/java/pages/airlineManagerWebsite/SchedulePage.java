79b4cfa51f6a3226cc49ead3690faafee1c36fbd
Dmytro Zubenko <fokist@outlook.com>
2023-04-30 15:18:52 +0300

    Gather on route plane info logic implemented

--- src/main/java/pages/airlineManagerWebsite/SchedulePage.java
+++ src/main/java/pages/airlineManagerWebsite/SchedulePage.java
@@ -1,29 +1,33 @@
 package pages.airlineManagerWebsite;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
 import java.util.List;
-import java.util.Objects;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class SchedulePage {
+    String loading = "planningLoadingBar";
     String sortedTableRows = "sortable";
     String aircraftModel = "blueText";
     String aircraftRouteName = "aircraftUserName";
-    String aircraftId = "//td/a";
-    String wavesPerPlane = "//span[@class='center']";
+    String aircraftName = "aircraftName";
+    String aircraftId = "a";
+    String wavesPerPlane = "center";
 
     public void collectInfoAboutPlanesAndWavesOnTheRoute(List<List<String>> collectedData) {
+        $(By.className(loading)).shouldBe(Condition.hidden);
         ElementsCollection aircraft = $$(By.className(sortedTableRows));
 
         for (SelenideElement row : aircraft) {
             String model = row.$(By.className(aircraftModel)).getText();
             String roteName = row.$(By.className(aircraftRouteName)).getText();
-            String aircraftID = Objects.requireNonNull(row.$x(aircraftId).getAttribute("href")).replaceAll("\\D+", "");
-            String howManyWavesIsCarried = String.valueOf(row.$$x(wavesPerPlane).size());
+            String aircraftID = row.$(By.className(aircraftName)).find(By.tagName(aircraftId)).getAttribute("href").replaceAll("\\D+", "");
+            String howManyWavesIsCarried = String.valueOf(row.$$(By.className(wavesPerPlane)).size());
             collectedData.add(List.of(model, roteName, aircraftID, howManyWavesIsCarried));
         }
     }
