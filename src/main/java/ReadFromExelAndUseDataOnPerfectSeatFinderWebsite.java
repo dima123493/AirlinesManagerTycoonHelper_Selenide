fb2dd6f255e2ea3e57a3dee545c206af1516bea5
Dmytro Zubenko <fokist@outlook.com>
2023-06-18 17:10:56 +0300

    Route Time collector implemented

--- src/main/java/ReadFromExelAndUseDataOnPerfectSeatFinderWebsite.java
+++ src/main/java/ReadFromExelAndUseDataOnPerfectSeatFinderWebsite.java
@@ -6,9 +6,6 @@ import pages.finderWebsite.PerfectSeatFinderPage;
 import propertyLoader.AirlinesProperties;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -22,21 +19,22 @@ public class ReadFromExelAndUseDataOnPerfectSeatFinderWebsite {
     static final String CRJ_1000_MANUFACTURER_NAME = "Bombardier";
     static final String CRJ_1000_MODEL_NUMBER = "CRJ-1000 ";
     static final int CRJ_1000_MAX_DISTANCE = 3129;
+
     //Middle haul
+    //Long haul
     static final String A300_600R_MANUFACTURER_NAME = "Airbus";
     static final String A300_600R_MODEL_NUMBER = "A300-600R ";
     static final int A300_600R_MAX_DISTANCE = 7540;
-    //Long haul
     static final String A380_800_MANUFACTURER_NAME = "Airbus";
     static final String A380_800_MODEL_NUMBER = "A380-800 ";
     static final int A380_800_MAX_DISTANCE = 15556;
 
     public static void main(String[] args) throws IOException {
-        var dataFromExcel = ReadFile.readGatheredInfoFile("AirlinesManagerAllRoutesInfo");//or AirlinesManagerNewRoutesInfo
-        List<List<String>> dataFromPerfectSeatFinderSite = new ArrayList<>();
+        var dataFromExcel = ReadFile.readGatheredInfoFile("AirlinesManagerAllRoutesInfo1");//or AirlinesManagerNewRoutesInfo
+        List<List<String>> dataFromPerfectSeatFinderSite = new LinkedList<>();
 
-        var file2 = Path.of(getProperty("file-path-to-distance-for-each-route"));//this can be removed if new version works
-        List<String> kilometers = Files.readAllLines(file2);//this can be removed if new version works
+        //var file2 = Path.of(getProperty("file-path-to-distance-for-each-route"));//this can be removed if new version works
+        //List<String> kilometers = Files.readAllLines(file2);//this can be removed if new version works
 
         Configuration.browserSize = "1920x1080";
         open(URL);
@@ -46,7 +44,7 @@ public class ReadFromExelAndUseDataOnPerfectSeatFinderWebsite {
         perfectSeatFinderPage.changeHubInputType(SOURCE_HUB_VALUE);
 
         for (var row : dataFromExcel) {
-            int routeDistance = Integer.parseInt(kilometers.get(dataFromExcel.indexOf(row)));//this can be removed if new version works
+            //int routeDistance = Integer.parseInt(kilometers.get(dataFromExcel.indexOf(row)));//this can be removed if new version works
             int routeDistanceFromExcel = Integer.parseInt(row.distanceToAirport());//new version
 
             if (routeDistanceFromExcel <= CRJ_1000_MAX_DISTANCE) {
@@ -66,15 +64,21 @@ public class ReadFromExelAndUseDataOnPerfectSeatFinderWebsite {
         open(AirlinesProperties.getProperty("route-planing-page"));
         RoutePlaningPage routeTime = new RoutePlaningPage();
         List<String> time = new LinkedList<>();
+        routeTime.getAirCrafts();
+        String airCraftIdNeeded = "";
+        String airCraftIdSelected = "";
         for (var row : dataFromExcel) {
-            int routeDistance = Integer.parseInt(kilometers.get(dataFromExcel.indexOf(row)));//this can be removed if new version works
-            int routeDistanceFromExcel = Integer.parseInt(row.distanceToAirport());//new version
-            if (routeDistanceFromExcel <= CRJ_1000_MAX_DISTANCE) {
-                routeTime.getTimeForTheRouteWave(time,CRJ_1000_MODEL_NUMBER, row.destinationHubValue());
-            } else if (routeDistanceFromExcel > CRJ_1000_MAX_DISTANCE && routeDistanceFromExcel <= A300_600R_MAX_DISTANCE) {
-                routeTime.getTimeForTheRouteWave(time,A300_600R_MODEL_NUMBER, row.destinationHubValue());
+            //int routeDistance = Integer.parseInt(kilometers.get(dataFromExcel.indexOf(row)));//this can be removed if new version works
+            int routeDistanceFromExcel = Integer.parseInt(row.distanceToAirport());//new version .distanceToAirport()
+            if (!row.destinationHubValue().contains("GIB") && routeDistanceFromExcel <= CRJ_1000_MAX_DISTANCE) {
+                airCraftIdNeeded =  routeTime.getAircraftId(CRJ_1000_MODEL_NUMBER);
+                airCraftIdSelected = routeTime.getTimeForTheRouteWave(time, row.destinationHubValue(),airCraftIdNeeded,airCraftIdSelected);
+            } else if (routeDistanceFromExcel > CRJ_1000_MAX_DISTANCE && routeDistanceFromExcel <= A300_600R_MAX_DISTANCE || row.destinationHubValue().equals("GIB")) {
+                airCraftIdNeeded =  routeTime.getAircraftId(A300_600R_MODEL_NUMBER);
+                airCraftIdSelected = routeTime.getTimeForTheRouteWave(time, row.destinationHubValue(),airCraftIdNeeded,airCraftIdSelected);
             } else if (routeDistanceFromExcel > A300_600R_MAX_DISTANCE && routeDistanceFromExcel <= A380_800_MAX_DISTANCE) {
-                routeTime.getTimeForTheRouteWave(time,A380_800_MODEL_NUMBER, row.destinationHubValue());
+                airCraftIdNeeded =  routeTime.getAircraftId(A380_800_MODEL_NUMBER);
+                airCraftIdSelected = routeTime.getTimeForTheRouteWave(time, row.destinationHubValue(),airCraftIdNeeded,airCraftIdSelected);
             }
         }
 
@@ -82,25 +86,8 @@ public class ReadFromExelAndUseDataOnPerfectSeatFinderWebsite {
             dataFromPerfectSeatFinderSite.get(i).add(time.get(i));
         }
 
-        WriteFile.writeInfoFromPerfectSeatFinderWebsite(dataFromPerfectSeatFinderSite,"PerfectSeatFinderAllRoutes");
+        WriteFile.writeInfoFromPerfectSeatFinderWebsite(dataFromPerfectSeatFinderSite, "PerfectSeatFinderAllRoutesInfo");//PerfectSeatFinderNewRoutesInfo
         closeWebDriver();
-
-/*        List<List<String>>listOfSeatFinder = new ArrayList<>();
-        listOfSeatFinder.add(new ArrayList<>(List.of("field1","field2","field3")));
-        listOfSeatFinder.add(new ArrayList<>(List.of("field2","field2","field3")));
-        listOfSeatFinder.add(new ArrayList<>(List.of("field3","field2","field3")));
-        listOfSeatFinder.add(new ArrayList<>(List.of("field4","field2","field3")));
-
-        List<String> time = new LinkedList<>();
-        time.add("5:45");
-        time.add("4:11");
-        time.add("2:15");
-        time.add("3:12");
-
-            for (int i = 0; i < listOfSeatFinder.size(); i++) {
-                listOfSeatFinder.get(i).add(time.get(i));
-                System.out.println(listOfSeatFinder.get(i));
-            }*/
 
     }
 
--- src/main/java/pages/airlineManagerWebsite/RoutePlaningPage.java
+++ src/main/java/pages/airlineManagerWebsite/RoutePlaningPage.java
@@ -1,24 +1,120 @@
 package pages.airlineManagerWebsite;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
-import java.util.List;
+import java.util.*;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class RoutePlaningPage {
-    String aircraftModel = "//*[@class=\"bold\"]";
-    String searchAirportNameField = "lineNameFilter";
-    String nameOfSelectedAirport = "//*[@id=\"lineList\"]/span";
+    private static final String CAROUSEL_VIEW = "carouselView";
+    private static final String collectionOfAirCrafts = "//*[@id=\"carouselView\"]/div/div";
+    private static final String aircraftModel = "//span[@class=\"bold\"]";
+    private static final String searchAirportNameField = "lineNameFilter";
+    private static final String nameOfSelectedAirport = "//*[@id=\"lineList\"]/span";
+    private static final String allDistances = "//input[@value='all']";
+    private static final String shDistance = "//input[@value='cc']";
+    private static final String mhDistance = "//input[@value='mc']";
+    private static final String lhDistance = "//input[@value='lc']";
 
-    public void getTimeForTheRouteWave(List<String> time, String model,String destinationHubValue){
-        SelenideElement airCraft = $$x(aircraftModel).filter(Condition.have(Condition.text(model))).first();
-        airCraft.click();
-        $(By.id(searchAirportNameField)).setValue(destinationHubValue);
-        String timeFormatted = $x(nameOfSelectedAirport).getText().replaceAll(".*(?=) ","").trim().replace("h",":");
-        time.add(timeFormatted);
-        System.out.println(timeFormatted);
+    private static final SelenideElement scrollLineIndicator = $x("//*[@id=\"aircraftSlider\"]/a");
+    private static final SelenideElement scrollToTheRight = $(By.id("aircraftSliderRight"));
+    private static final SelenideElement scrollToTheLeft = $(By.id("aircraftSliderLeft"));
+
+    public Map<String, String> airCrafts = new LinkedHashMap<>();
+
+    public void getAirCrafts() {
+        $(By.id(CAROUSEL_VIEW)).shouldBe(Condition.visible);
+        ElementsCollection airCraftCollection = $$x(collectionOfAirCrafts);
+        List<String> locators = new ArrayList<>();
+        locators.add(shDistance);
+        locators.add(mhDistance);
+        locators.add(lhDistance);
+        for (String amount : locators) {
+            $x(amount).scrollTo().click();
+            for (SelenideElement aircraft : airCraftCollection) {
+                String aircraftId = aircraft.getAttribute("id");//.replaceAll("\\D+", "")
+                String aircrftModel = aircraft.findElement(By.xpath(aircraftModel)).getText().replaceAll("(?<= ).*", "").trim();
+                airCrafts.put(aircraftId, aircrftModel);
+            }
+        }
+        $x(allDistances).scrollTo().click();
     }
+
+    public String getAircraftId(String model) {
+        String aircraftId = "";
+        for (Map.Entry<String, String> value : airCrafts.entrySet()) {
+            if (value.getValue().contains(model.trim())) {
+                aircraftId = value.getKey();
+                break;
+            }
+        }
+        return aircraftId;
+    }
+
+    public String getTimeForTheRouteWave(List<String> time, String destinationHubValue, String airCraftIdNeeded, String airCraftIdSelected) {
+        //System.out.println(airCrafts);
+        /*Map.Entry<String, String> airCraft = airCrafts.entrySet().stream().findFirst().filter(el -> !el.getValue().contains(model)).get();
+        String aircraftId = airCraft.getKey();*/
+        /*String aircraftId = "";
+        for(Map.Entry<String, String> value : airCrafts.entrySet()){
+            if(value.getValue().contains(model.trim())){
+                aircraftId = value.getKey();
+                break;
+            }
+        }*/
+
+        boolean selected = false;
+        String selectedAircraftId = "";
+
+
+        while (!selected) {
+            if (!airCraftIdSelected.equals(airCraftIdNeeded)) {
+                scrollToTheRight.click();
+                if ($(By.id(airCraftIdNeeded)).isDisplayed()) {
+                    $(By.id(airCraftIdNeeded)).click();
+                    selectedAircraftId = airCraftIdNeeded;
+                    selected = true;
+                    rollBackCursor();
+                }
+            } else {
+                selectedAircraftId = airCraftIdNeeded;
+                selected = true;
+            }
+            if (Objects.equals(scrollLineIndicator.getAttribute("style"), "left: 100%;")) {
+                selected = true;
+                System.out.println("Plane not found or check the name search");
+            }
+        }
+
+
+        $(By.id(searchAirportNameField)).scrollTo().setValue(destinationHubValue);
+        String timeFormatted = $x(nameOfSelectedAirport).shouldBe(Condition.appear).getText().replaceAll(".*(?=) ", "").trim().replace("h", ":");
+        time.add(timeFormatted);
+
+        return selectedAircraftId;
+    }
+
+    public void rollBackCursor() {
+        boolean returned = false;
+        while (!returned) {
+            scrollToTheLeft.click();
+            if (Objects.equals(scrollLineIndicator.getAttribute("style"), "left: 0%;")) {
+                returned = true;
+            }
+        }
+    }
+
+/*    public void pickDistanceRange(String aircraftRange) {
+        if (aircraftRange.equals("SH")) {
+            $x(shDistance).scrollTo().click();
+        } else if (aircraftRange.equals("MH")) {
+            $x(mhDistance).scrollTo().click();
+        } else {
+            $x(lhDistance).scrollTo().click();
+        }
+    }*/
 }
